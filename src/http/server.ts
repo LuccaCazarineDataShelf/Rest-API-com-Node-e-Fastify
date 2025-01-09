d4b1d1d98a01b6f465994c5f9721893c8650304b
Lucca Cazarine <luccacazarine@gmail.com>
2025-01-09 15:25:43 -0300

    add banco

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 
 const app = fastify()
 
+app.get('/', () => {
+    return 'Servidor ok'
+})
+
 app.listen({port: 8080}).then(() => {
     console.log('Server is running')
 })
